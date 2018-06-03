9d242467fa5fb3af6d5cf8f04ef3aa29a946df9a
Amaury Martiny <amaury.martiny@protonmail.com>
2018-06-03 14:16:24 +0200

    Better electron index

--- packages/crale-electron/src/main/index.js
+++ packages/crale-electron/src/main/index.js
@@ -1,8 +1,11 @@
 import { app, BrowserWindow } from 'electron';
 
+// Global reference to mainWindow (necessary to prevent window from being garbage collected)
+let mainWindow;
+
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
   // By default opens file:///path/to/crale-react/build/index.html, unless
   // there's a ENV variable set, which we will use for development.
@@ -24,4 +27,22 @@ const createWindow = () => {
   );
 };
 
-app.on('ready', createWindow);
+// Quit application when all windows are closed
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications to stay open until the user explicitly quits
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it is common to re-create a window even after all windows have been closed
+  if (mainWindow === null) {
+    mainWindow = createMainWindow();
+  }
+});
+
+// Create main BrowserWindow when electron is ready
+app.on('ready', () => {
+  mainWindow = createMainWindow();
+});
